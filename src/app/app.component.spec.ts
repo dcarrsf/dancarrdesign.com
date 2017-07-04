d593d2212bd6220c2d9cc793ead11426377ef4da
dcarrsf <dan@dancarr.co>
2017-07-04 12:40:33 -0700

    Fixing broken path to services.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { provideRoutes } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { ApiService } from './shared';
+import { ApiService } from './shared/services/';
 import { AppComponent } from './app.component';
 
 describe('App', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ApiService } from './shared';
+import { ApiService } from './shared/services/';
 
 // External CSS
 import '../style/app.scss';
