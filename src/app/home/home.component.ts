32ea43362871370895726f1701521e67af29afcc
dcarrsf <dan@dancarr.co>
2017-07-05 23:34:56 -0700

    Shaping out home pag eand navigation links.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../shared/services/';
 
 @Component({
-  selector: 'my-home',
+  selector: 'home-tag',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  private banner: string;
 
-  constructor() {
-    // Do stuff
-  }
+  constructor(private api: ApiService) {}
 
   ngOnInit() {
-
+    this.banner = this.api.strings.banner;
   }
 }
--- src/app/shared/components/logo/logo.component.ts
+++ src/app/shared/components/logo/logo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../shared/services/';
 
 @Component({
   selector: 'logo-tag',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logo.component.scss']
 })
 export class LogoComponent implements OnInit {
+  // Properties
+  logo: string;
 
-  constructor() { }
+  // Constructor
+  constructor(private api: ApiService) { }
 
+  // Lifecycle
   ngOnInit() {
-
+    this.logo = this.api.strings.logo;
   }
 }
