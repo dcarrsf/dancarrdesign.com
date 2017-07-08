e8d5a04c1bfc3388035ada3781ee8345e957d1a5
dcarrsf <dan@dancarr.co>
2017-07-08 11:04:02 -0700

    Adding background directive to control the styling of the background image.

---
+++ src/app/shared/directives/background/background.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BackgroundDirective } from './background.directive';
+
+describe('Directive: Background', () => {
+  it('should create an instance', () => {
+    const directive = new BackgroundDirective();
+    expect(directive).toBeTruthy();
+  });
+});
